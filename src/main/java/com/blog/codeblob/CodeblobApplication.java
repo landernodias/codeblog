aeb71b5444119f775cf0980f4c7e31cc99e2df10
Nelcione Dias <nelcionedias@gmail.com>
2022-11-15 10:02:50 -0300

    initialize project

---
+++ src/main/java/com/blog/codeblob/CodeblobApplication.java
@@ -0,0 +1,13 @@
+package com.blog.codeblob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeblobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeblobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/codeblob/CodeblobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.codeblob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeblobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
